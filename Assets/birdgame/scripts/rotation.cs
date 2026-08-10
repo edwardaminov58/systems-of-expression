7e3de81f6f423f399fdbec395d24b7f18b9a31c5
edwardaminov58 <edwardaminov58@gmail.com>
2026-08-10 08:14:56 -0400

    camera tilt l4

--- Assets/birdgame/scripts/rotation.cs
+++ Assets/birdgame/scripts/rotation.cs
@@ -57,6 +57,12 @@ public class rotation : MonoBehaviour
                // vcam.transform.rotation = Quaternion.Euler(Mathf.Lerp(15, 0, t), vcam.transform.rotation.y, vcam.transform.rotation.z);
                 cameraTilt = Mathf.Lerp(15, -2.5f, t);
             }
+            else if (AltitudeManager.currentHeightLayer == 3)
+            {
+                float t = (gameObject.transform.localPosition.y - altStart) / cameraTiltThreshold;
+               // vcam.transform.rotation = Quaternion.Euler(Mathf.Lerp(15, 0, t), vcam.transform.rotation.y, vcam.transform.rotation.z);
+                cameraTilt = Mathf.Lerp(-2.5f, 0, t);
+            }
          
             
         }
